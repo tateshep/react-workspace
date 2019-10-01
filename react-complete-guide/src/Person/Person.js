ef202b8c71dc6fe39a9429417e6117addf76cbb6
Tate Shepherd <tateshep@gmail.com>
2019-09-30 17:13:06 -0700

    update after ejecting

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -5,7 +5,7 @@ import './Person.css';
 const person = (props) => {
    
     return (
-        <div className="Person" style={style}>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name}, and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
